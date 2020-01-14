1e6acf89df743912cc292a5e1c5b1674388a485b
Martin Kellogg <kelloggm@cs.washington.edu>
2020-01-14 15:15:49 -0800

    Initial commit

---
+++ src/main/java/org/checkerframework/checker/templatefora/TemplateforaChecker.java
@@ -0,0 +1,8 @@
+package org.checkerframework.checker.templatefora;
+
+import org.checkerframework.common.basetype.BaseTypeChecker;
+
+/**
+ * This is the entry point for pluggable type-checking.
+ */
+public class TemplateforaChecker extends BaseTypeChecker {}
